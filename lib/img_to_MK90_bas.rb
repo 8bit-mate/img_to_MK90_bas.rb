630f2a64ea441d15414fd4dbb8dcd449fb044e8c
Mate <mate@example.com>
2023-03-09 15:03:51 +0700

    initial commit

---
+++ lib/img_to_MK90_bas.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "img_to_MK90_bas/version"
+
+module ImgToMK90Bas
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/img_to_MK90_bas/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ImgToMK90Bas
+  VERSION = "0.1.0"
+end
